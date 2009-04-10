c807a63c97a049fc1e21b9cbaa25d0e7665488c5
mdonohue <mdonohue@dev.java.net>
2009-04-10 05:11:55 +0000

    Remove useless imports

--- src/main/java/configurationslicing/Slice.java
+++ src/main/java/configurationslicing/Slice.java
@@ -1,15 +1,9 @@
 package configurationslicing;
 
+import hudson.model.Descriptor.FormException;
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.StaplerRequest;
-
-import hudson.ExtensionPoint;
-import hudson.maven.MavenReporterDescriptor;
-import hudson.model.Describable;
-import hudson.model.Descriptor;
-import hudson.model.Hudson;
-import hudson.model.Descriptor.FormException;
 
 public abstract class Slice{
     public abstract Slice newInstance(StaplerRequest req, JSONObject formData)
