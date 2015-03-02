2523774125cc54319bf4c6ed5ddd888b282f4837
ninian <ninian_harvey@yahoo.com>
2015-03-02 18:19:22 +0000

    Applying suggested fix for JENKINS-25964

--- src/main/java/configurationslicing/UnorderedStringSlice.java
+++ src/main/java/configurationslicing/UnorderedStringSlice.java
@@ -13,6 +13,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
@@ -20,9 +21,13 @@ import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
 
 import configurationslicing.UnorderedStringSlicer.UnorderedStringSlicerSpec;
+import configurationslicing.tools.AbstractToolSlicerSpec;
 
 public class UnorderedStringSlice<I> extends Slice {
+    
+    private static final Logger LOGGER = Logger.getLogger(UnorderedStringSlice.class.getName());
     private Map<String, List<String>> nameToValues;
+    
     private Map<String, Set<String>> valueToNames;
     private UnorderedStringSlicer.UnorderedStringSlicerSpec<I> spec;
     
@@ -75,6 +80,11 @@ public class UnorderedStringSlice<I> extends Slice {
     }
 
     private static void addLineWithSets(Map<String, Set<String>> map, String s, String name) {
+        if (null == s) {
+            LOGGER.severe("found illegal line with null value for name: "+name);
+            // do nothing
+            return;
+        }
         if(!map.containsKey(s)) {
             map.put(s, new HashSet<String>());
         }
