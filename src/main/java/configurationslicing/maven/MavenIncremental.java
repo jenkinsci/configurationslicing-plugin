906212f395876ea9386c5d6c86886fc2c7b4ba0b
Michael Donohue <michael.donohue@gmail.com>
2016-02-10 22:31:58 -0800

    Config slice for Maven 'incremental' setting

---
+++ src/main/java/configurationslicing/maven/MavenIncremental.java
@@ -0,0 +1,50 @@
+package configurationslicing.maven;
+
+import hudson.Extension;
+import hudson.maven.MavenModuleSet;
+
+import java.util.List;
+
+import jenkins.model.Jenkins;
+import configurationslicing.BooleanSlicer;
+
+@Extension
+public class MavenIncremental extends BooleanSlicer<MavenModuleSet> {
+
+    public MavenIncremental() {
+        super(new MavenIncrementalSlicerSpec());
+    }
+    
+    public static class MavenIncrementalSlicerSpec implements BooleanSlicerSpec<MavenModuleSet> {
+
+        public String getName() {
+            return "Maven Incremental Build";
+        }
+
+        public String getName(MavenModuleSet item) {
+            return item.getFullName();
+        }
+
+        public String getUrl() {
+            return "mavenincremental";
+        }
+
+        @SuppressWarnings("unchecked")
+		public List<MavenModuleSet> getWorkDomain() {
+            return (List) Jenkins.getInstance().getAllItems(MavenModuleSet.class);
+        }
+
+		@Override
+		public boolean getValue(MavenModuleSet item) {
+			return item.isIncrementalBuild();
+		}
+
+		@Override
+		public boolean setValue(MavenModuleSet item, boolean value) {
+			item.setIncrementalBuild(value);
+			return true;
+		}
+        
+    }
+}
+ 
