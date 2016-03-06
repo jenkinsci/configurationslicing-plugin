bd30068028136c724b6b98d53ff0ba4bd40b8584
Michael Donohue <michael.donohue@gmail.com>
2016-03-06 12:27:03 -0800

    Change build timeout slicer to use XStream
    The build timeout slicer has introduced a more object oriented
    configuration with timeout strategies, which is difficult
    for the slicer to follow in a static way. So instead the slicer
    now leverages the XStream serializer, which should match any updates
    to the build timeout plugin, at the expense of a more verbose
    slice language.
    
    Also, the option to slice more narrowly, such as BuildTimeoutSlicer2
    provided is no longer trivially possible.  Something like XPath might
    be useful to make that happen in the future though.

--- src/main/java/configurationslicing/buildtimeout/BuildTimeoutSlicer.java
+++ src/main/java/configurationslicing/buildtimeout/BuildTimeoutSlicer.java
@@ -7,175 +7,156 @@ import hudson.model.Descriptor;
 import hudson.plugins.build_timeout.BuildTimeoutWrapper;
 import hudson.tasks.BuildWrapper;
 import hudson.util.DescribableList;
+import hudson.util.XStream2;
 
 import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.apache.commons.lang.StringUtils;
+
+import com.thoughtworks.xstream.XStreamException;
 
 import configurationslicing.TopLevelItemSelector;
 import configurationslicing.UnorderedStringSlicer;
 
 @Extension
 public class BuildTimeoutSlicer extends UnorderedStringSlicer<AbstractProject<?,?>>{
+    private static final Logger LOGGER = Logger.getLogger(BuildTimeoutSlicer.class.getName());
 
-	private static final String DEFAULT_TYPE = "absolute";
-	
     public BuildTimeoutSlicer() {
         super(new BuildTimeoutSliceSpec());
     }
 
-	@Override
+    @Override
     public boolean isLoaded() {
-    	try {
-    		BuildTimeoutSliceSpec.newBuildTimeoutWrapper(0, false, DEFAULT_TYPE);
-    		return true;
-    	} catch (Throwable t) {
-    		return false;
-    	}
+        try { 
+            BuildTimeoutWrapper.class.toString();
+            return true;
+        } catch (Throwable t) {
+            return false;
+        }
     }
 
     public static class BuildTimeoutSliceSpec extends UnorderedStringSlicerSpec<AbstractProject<?,?>> {
 
         private static final String DISABLED = "(Disabled)";
-        private static final String SEPARATOR = ",";
 
-		@Override
-		public String getName() {
-			return "Build Timeout";
-		}
+        @Override
+        public String getName() {
+            return "Build Timeout";
+        }
 
-		@Override
-		public String getUrl() {
-			return "buildtimeout";
-		}
+        @Override
+        public String getUrl() {
+            return "buildtimeout";
+        }
 
-		@Override
-		public List<AbstractProject<?, ?>> getWorkDomain() {
+        @Override
+        public List<AbstractProject<?, ?>> getWorkDomain() {
             return TopLevelItemSelector.getAllTopLevelItems(BuildableItemWithBuildWrappers.class);
-		}
+        }
 
-		@Override
-		public List<String> getValues(AbstractProject<?, ?> item) {
-			BuildableItemWithBuildWrappers bi = (BuildableItemWithBuildWrappers) item;
-			DescribableList<BuildWrapper,Descriptor<BuildWrapper>> wrappers = bi.getBuildWrappersList();
-        	List<String> values = new ArrayList<String>();
-			BuildTimeoutWrapper wrapper = wrappers.get(BuildTimeoutWrapper.class);
-			if (wrapper != null) {
-				String value = wrapper.timeoutMinutes + SEPARATOR + wrapper.failBuild;
-				values.add(value);
-			}
-			if (values.isEmpty()) {
-				values.add(DISABLED);
-			}
-			return values;
-		}
+        @Override
+        public List<String> getValues(AbstractProject<?, ?> item) {
+            XStream2 xs = getXStream();
+            BuildableItemWithBuildWrappers bi = (BuildableItemWithBuildWrappers) item;
+            DescribableList<BuildWrapper,Descriptor<BuildWrapper>> wrappers = bi.getBuildWrappersList();
+            List<String> values = new ArrayList<String>();
+            BuildTimeoutWrapper wrapper = wrappers.get(BuildTimeoutWrapper.class);
+            if (wrapper != null) {
+                StringWriter sw = new StringWriter();
+                xs.toXML(wrapper, sw);
+                String value = sw.toString();
 
-		@Override
-		public boolean setValues(AbstractProject<?, ?> item, List<String> set) {
-			BuildableItemWithBuildWrappers bi = (BuildableItemWithBuildWrappers) item;
-			DescribableList<BuildWrapper,Descriptor<BuildWrapper>> wrappers = bi.getBuildWrappersList();
-			boolean changed = false;
-			BuildTimeoutWrapper wrapper = wrappers.get(BuildTimeoutWrapper.class);
+                values.add(value);
+            }
+            if (values.isEmpty()) {
+                values.add(DISABLED);
+            }
+            return values;
+        }
 
-			boolean delete = false;
-			boolean newFail = false;
-			String oldType = DEFAULT_TYPE;
-			int newTimeout = 0;
-			String line = set.iterator().next();
-			if (DISABLED.equals(line) || StringUtils.isEmpty(line)) {
-				delete = true;
-			} else {
-				String[] split = line.split(SEPARATOR);
-				newTimeout = Integer.parseInt(split[0]);
-				newFail = Boolean.parseBoolean(split[1]);
-			}
+        private XStream2 getXStream() {
+            XStream2 xs = new XStream2();
+            addSimpleAlias(xs, "hudson.plugins.build_timeout.BuildTimeoutWrapper");
+            addSimpleAlias(xs, "hudson.plugins.build_timeout.impl.AbsoluteTimeOutStrategy");
+            addSimpleAlias(xs, "hudson.plugins.build_timeout.impl.DeadlineTimeOutStrategy");
+            addSimpleAlias(xs, "hudson.plugins.build_timeout.impl.ElasticTimeOutStrategy");
+            addSimpleAlias(xs, "hudson.plugins.build_timeout.impl.LikelyStuckTimeOutStrategy");
+            addSimpleAlias(xs, "hudson.plugins.build_timeout.impl.NoActivityTimeOutStrategy");
+            return xs;
+        }
 
-			if (wrapper != null) {
-				boolean oldFail = wrapper.failBuild;
-				int oldTimeout = wrapper.timeoutMinutes;
-				oldType = getType(wrapper);
-				if (newFail != oldFail || newTimeout != oldTimeout) {
-					changed = true;
-				}
-			} else {
-				changed = true;
-			}
-			
-			if (delete) {
-				if (wrapper != null) {
-				    wrappers.remove(wrapper);
-				}
-			} else if (changed) {
-				try {
-					wrapper = newBuildTimeoutWrapper(newTimeout, newFail, oldType);
-					wrappers.replace(wrapper);
-				} catch (IOException e) {
-					System.err.println("IOException Thrown replacing wrapper value");
-					return false;
-				}
-			}
-			
-			return changed;
-		}
+        private void addSimpleAlias(XStream2 xs, String className) {
+            Class<?> type;
+            try {
+                type = Class.forName(className);
+                String name = type.getSimpleName();
+                xs.alias(name, type);
+            } catch (ClassNotFoundException e) {
+                LOGGER.info("Cannot load " + className);
+            }
+        }
 
-		@Override
-		public String getName(AbstractProject<?, ?> item) {
+        @Override
+        public boolean setValues(AbstractProject<?, ?> item, List<String> set) {
+            LOGGER.info("BuildTimeoutSlicer.setValues for item " + item.getName());
+            XStream2 xs = getXStream();
+            BuildableItemWithBuildWrappers bi =
+                    (BuildableItemWithBuildWrappers) item;
+            DescribableList<BuildWrapper,Descriptor<BuildWrapper>> wrappers =
+                    bi.getBuildWrappersList();
+            boolean changed = false;
+            BuildTimeoutWrapper wrapper = wrappers.get(BuildTimeoutWrapper.class);
+            BuildTimeoutWrapper newWrapper = null;
+            boolean delete = false;
+            String line = set.iterator().next();
+            if (DISABLED.equals(line) || StringUtils.isEmpty(line)) {
+                delete = true;
+            } else {
+                try {
+                    Object o = xs.fromXML(line);
+                    if(o instanceof BuildTimeoutWrapper) {
+                        newWrapper = (BuildTimeoutWrapper)o;
+                        changed=true;
+                    }
+                } catch (XStreamException xse) {
+                    LOGGER.warning("XStreamException parsing XML for BuildTimeoutSlicer: " + xse.getMessage());
+                    changed=false;
+                }
+            }
+
+            if (delete) {
+                if (wrapper != null) {
+                    wrappers.remove(wrapper);
+                    changed=true;
+                }
+            } else if (newWrapper != null && changed) {
+                try {
+                    wrappers.replace(newWrapper);
+                } catch (IOException e) {
+                    LOGGER.warning("IOException Thrown replacing wrapper value");
+                    return false;
+                }
+            }
+
+            return changed;
+        }
+
+        @Override
+        public String getName(AbstractProject<?, ?> item) {
             return item.getFullName();
-		}
-		@Override
-		public String getDefaultValueString() {
-			return DISABLED;
-		}
-		@Override
-		public String getConfiguredValueDescription() {
-			return "Timeout,Fail<br/><i>(e.g. 180,false)</i>";
-		}
-		public static String getType(BuildTimeoutWrapper wrapper) {
-			try {
-				Field field = BuildTimeoutWrapper.class.getDeclaredField("timeoutType");
-				String value = (String) field.get(wrapper);
-				return value;
-			} catch (Exception e) {
-				return DEFAULT_TYPE;
-			}
-		}
-		public static BuildTimeoutWrapper newBuildTimeoutWrapper(int timeoutMinutes, boolean failBuild, String type) {
-			//	(int timeoutMinutes, boolean failBuild)
-			//	(int timeoutMinutes, boolean failBuild, boolean writingDescription, int timeoutPercentage, int timeoutMinutesElasticDefault, String timeoutType)
-			
-			Class<BuildTimeoutWrapper> cls = BuildTimeoutWrapper.class;
-			Class<?>[] types1 = new Class<?>[] { Integer.TYPE, Boolean.TYPE };
-			Constructor<BuildTimeoutWrapper> cons;
-			try {
-				cons = cls.getDeclaredConstructor(types1);
-			} catch (Exception e) {
-				cons = null;
-			}
-			
-			Object[] args;
-			if (cons != null) {
-				args = new Object[] { timeoutMinutes, failBuild };
-			} else {
-				try {
-					Class<?>[] types2 = new Class<?>[] { Integer.TYPE, Boolean.TYPE, Boolean.TYPE, Integer.TYPE, Integer.TYPE, String.class };
-					cons = cls.getDeclaredConstructor(types2);
-					args = new Object[] { timeoutMinutes, failBuild, false, 0, 0, type };
-				} catch (Exception e) {
-					throw new UnsupportedClassVersionError("Cannot find a version of BuildTimeoutWrapper constructor that can be used:" + e.getMessage());
-				}
-			}
-			BuildTimeoutWrapper wrapper;
-			try {
-				wrapper = (BuildTimeoutWrapper) cons.newInstance(args);
-			} catch (Exception e) {
-				throw new UnsupportedClassVersionError("Cannot find a version of BuildTimeoutWrapper constructor that can be used:" + e.getMessage());
-			}
-			return wrapper;
-		}
-		
+        }
+        @Override
+        public String getDefaultValueString() {
+            return DISABLED;
+        }
+        @Override
+        public String getConfiguredValueDescription() {
+            return "Build Timeout XML";
+        }
     }
 }
