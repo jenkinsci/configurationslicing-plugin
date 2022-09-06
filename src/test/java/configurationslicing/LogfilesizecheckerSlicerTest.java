480d3f383e6d0508c0c8e3b3263332c408707912
Mark Waite <mark.earl.waite@gmail.com>
2022-09-06 12:55:35 -0600

    Use JenkinsRule in test to assure initialization works
    Seems to have had conditions previously that did not always require a
    JenkinsRule.
    
    Converts the test from JUnit 3 to JUnit 4.

--- src/test/java/configurationslicing/LogfilesizecheckerSlicerTest.java
+++ src/test/java/configurationslicing/LogfilesizecheckerSlicerTest.java
@@ -2,20 +2,30 @@ package configurationslicing;
 
 import configurationslicing.logfilesizechecker.LogfilesizecheckerSlicer;
 import hudson.plugins.logfilesizechecker.LogfilesizecheckerWrapper;
-import junit.framework.TestCase;
 
-public class LogfilesizecheckerSlicerTest extends TestCase {
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
-	int maxLogSize = 3; 
+import static org.junit.Assert.*;
+
+public class LogfilesizecheckerSlicerTest {
+
+        @Rule public final JenkinsRule r = new JenkinsRule();
+
+        int maxLogSize = 3;
 	boolean failBuild = true;
 	boolean setOwn = true;
-	
+
+        @Test
 	public void testNewLogfilesizecheckerWrapper() {
 	    LogfilesizecheckerWrapper wrapper = LogfilesizecheckerSlicer.LogfilesizeSliceSpec.newLogfilesizecheckerWrapper(maxLogSize, failBuild, setOwn);
 		assertEquals(maxLogSize, wrapper.maxLogSize);
 		assertEquals(failBuild, wrapper.failBuild);
         assertEquals(setOwn, wrapper.setOwn);
 	}
+
+        @Test
 	public void testLogfilesizecheckerWrapperConstructor() {
 	    LogfilesizecheckerWrapper wrapper = new LogfilesizecheckerWrapper(maxLogSize, failBuild, setOwn);
 		assertEquals(maxLogSize, wrapper.maxLogSize);
