3ed84c31903f29838696b555453e6fae8fdc804a
Mark Waite <mark.earl.waite@gmail.com>
2025-10-25 11:43:35 -0600

    Use Apache commons collection4 and strictly declare dependencies
    Removes bundling of commons-collection

--- src/main/java/configurationslicing/maven/MavenVersionSlicer.java
+++ src/main/java/configurationslicing/maven/MavenVersionSlicer.java
@@ -18,8 +18,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import jenkins.model.Jenkins;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Predicate;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Predicate;
 
 @SuppressWarnings("unchecked")
 @Extension(optional = true)
