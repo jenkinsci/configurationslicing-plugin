9802345f82f4ae0059f53379ee0f8dc32255d949
jgyprime <jgyprime@gmail.com>
2026-04-06 18:42:05 +0300

    Add functionality to edit execute shell unstable code

---
+++ src/main/java/configurationslicing/executeshell/AbstractBuildUnstableReturnSlicer.java
@@ -0,0 +1,155 @@
+package configurationslicing.executeshell;
+
+import configurationslicing.UnorderedStringSlicer;
+import hudson.matrix.MatrixProject;
+import hudson.model.AbstractProject;
+import hudson.model.Descriptor;
+import hudson.model.Project;
+import hudson.tasks.Builder;
+import hudson.util.DescribableList;
+import java.util.ArrayList;
+import java.util.List;
+import jenkins.model.Jenkins;
+
+/**
+ * @author Jacob Robertson
+ */
+public abstract class AbstractBuildUnstableReturnSlicer<B extends Builder> extends UnorderedStringSlicer<AbstractProject> {
+
+    public AbstractBuildUnstableReturnSlicer(AbstractBuildUnstableReturnSliceSpec<B> spec) {
+        super(spec);
+    }
+
+    public abstract static class AbstractBuildUnstableReturnSliceSpec<B extends Builder>
+            extends UnorderedStringSlicerSpec<AbstractProject> {
+
+        public static final String NOTHING = "(nothing)";
+
+        public String getDefaultValueString() {
+            return NOTHING;
+        }
+
+        public String getName(AbstractProject item) {
+            return item.getFullName();
+        }
+
+        @Override
+        public boolean isIndexUsed(int count) {
+            return count > 1;
+        }
+
+        public List<String> getValues(AbstractProject item) {
+            List<String> content = new ArrayList<String>();
+            DescribableList<Builder, Descriptor<Builder>> buildersList = getBuildersList(item);
+
+            List<B> builders = getConcreteBuildersList(buildersList);
+            for (B builder : builders) {
+                content.add(getUnstableReturn(builder));
+            }
+            if (content.isEmpty()) {
+                content.add(NOTHING);
+            }
+
+            return content;
+        }
+
+        public abstract List<B> getConcreteBuildersList(DescribableList<Builder, Descriptor<Builder>> buildersList);
+
+        public abstract String getCommand(B builder);
+        
+        public abstract String getUnstableReturn(B builder);
+
+        public abstract void setUnstableReturn(B builder, int value);
+
+        public abstract B[] createBuilderArray(int len);
+
+        public abstract B createBuilder(String UnstableReturn, List<B> existingBuilders, B oldBuilder);
+
+        @SuppressWarnings("unchecked")
+        public List<AbstractProject> getWorkDomain() {
+            List<AbstractProject> list = new ArrayList<AbstractProject>();
+            List<AbstractProject> temp = Jenkins.get().getAllItems(AbstractProject.class);
+            for (AbstractProject p : temp) {
+                if (p instanceof Project || p instanceof MatrixProject) {
+                    list.add(p);
+                }
+            }
+            return list;
+        }
+
+        @SuppressWarnings("unchecked")
+        public static DescribableList<Builder, Descriptor<Builder>> getBuildersList(AbstractProject item) {
+            if (item instanceof Project<?, ?> project) {
+                return project.getBuildersList();
+            } else if (item instanceof MatrixProject project) {
+                return project.getBuildersList();
+            } else {
+                return null;
+            }
+        }
+
+        public boolean setValues(AbstractProject item, List<String> list) {
+            DescribableList<Builder, Descriptor<Builder>> buildersList = getBuildersList(item);
+            List<B> builders = getConcreteBuildersList(buildersList);
+
+            int maxLen = Math.max(list.size(), builders.size());
+            B[] oldBuilders = createBuilderArray(maxLen);
+            B[] newBuilders = createBuilderArray(maxLen);
+
+            for (int i = 0; i < builders.size(); i++) {
+                oldBuilders[i] = builders.get(i);
+            }
+
+            for (int i = 0; i < list.size(); i++) {
+                if (oldBuilders[i] != null) {
+                    newBuilders[i] = createBuilder(list.get(i), builders, oldBuilders[i]);
+                }
+            }
+
+            // perform any replacements
+            for (int i = 0; i < maxLen; i++) {
+                if (oldBuilders[i] != null && newBuilders[i] != null && oldBuilders[i] != newBuilders[i]) {
+                    replaceBuilder(buildersList, oldBuilders[i], newBuilders[i]);
+                }
+            }
+
+            // add any new ones (should always add to the end, but might not if the original UnstableReturn was empty)
+            for (int i = 0; i < maxLen; i++) {
+                if (oldBuilders[i] == null && newBuilders[i] != null) {
+                    buildersList.add(newBuilders[i]);
+                }
+            }
+
+            // delete any old ones
+            for (int i = 0; i < maxLen; i++) {
+                if (oldBuilders[i] != null && newBuilders[i] == null) {
+                    // the remove UnstableReturn will persist the project
+                    buildersList.remove(oldBuilders[i]);
+                }
+            }
+
+            return true;
+        }
+
+        /**
+         * If we do other builders, publishers, etc - this should be the pattern to use.
+         */
+        public static boolean replaceBuilder(
+                DescribableList<Builder, Descriptor<Builder>> builders, Builder oldBuilder, Builder newBuilder) {
+            List<Builder> newList = new ArrayList<Builder>(builders.toList());
+            for (int i = 0; i < newList.size(); i++) {
+                Builder b = newList.get(i);
+                if (b == oldBuilder) {
+                    newList.set(i, newBuilder);
+                }
+            }
+            try {
+                builders.replaceBy(newList);
+                return true;
+            } catch (java.io.IOException e) {
+                System.err.println("IOException Thrown replacing builder list");
+                return false;
+            }
+        }
+    }
+}
---
+++ src/main/java/configurationslicing/executeshell/ExecuteShellUnstableReturnSlicer.java
@@ -0,0 +1,85 @@
+package configurationslicing.executeshell;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.tasks.Builder;
+import hudson.tasks.Shell;
+import hudson.util.DescribableList;
+import java.util.List;
+
+/**
+ * Slicer for the shell builder
+ */
+@Extension
+public class ExecuteShellUnstableReturnSlicer extends AbstractBuildUnstableReturnSlicer<Shell> {
+
+    public ExecuteShellUnstableReturnSlicer() {
+        super(new ExecuteShellUnstableReturnSliceSpec());
+    }
+
+    public static class ExecuteShellUnstableReturnSliceSpec extends AbstractBuildUnstableReturnSliceSpec<Shell> {
+
+        public String getName() {
+            return "Execute shell unstable code slicer (beta)";
+        }
+
+        public String getUrl() {
+            return "executeshellunstableueturnslice";
+        }
+
+        @Override
+        public Shell createBuilder(String value, List<Shell> existingBuilders, Shell oldBuilder) {
+            String command = "# new shell";
+            if (oldBuilder != null) {
+                command = oldBuilder.getCommand();
+            }
+            Shell newBuilder = new Shell(command);
+            try {
+                int intValue = Integer.parseInt(value);
+                if (intValue > 0) {
+                    newBuilder.setUnstableReturn(intValue);
+                }
+            } catch (NumberFormatException e) {
+            }
+            if (oldBuilder != null) {
+                newBuilder.setConfiguredLocalRules(oldBuilder.getConfiguredLocalRules());
+            }
+
+            return newBuilder;
+        }
+
+        @Override
+        public Shell[] createBuilderArray(int len) {
+            return new Shell[len];
+        }
+
+        @Override
+        public String getCommand(Shell builder) {
+            return builder.getCommand();
+        }
+
+        @Override
+        public String getUnstableReturn(Shell builder) {
+            if (builder != null) {
+                Integer retval = builder.getUnstableReturn();
+                if(retval != null) {
+                    return retval.toString();
+                } else {
+                    return NOTHING;
+                }
+            } else {
+                return NOTHING;
+            }
+        }
+
+        @Override
+        public void setUnstableReturn(Shell builder, int value) {
+            builder.setUnstableReturn(value);
+        }
+
+        @Override
+        public List<Shell> getConcreteBuildersList(DescribableList<Builder, Descriptor<Builder>> buildersList) {
+            return buildersList.getAll(Shell.class);
+        }
+    }
+}
